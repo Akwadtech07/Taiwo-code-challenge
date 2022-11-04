f8f33a42c60a37c1798ebe7992e4da10fe1fbf3c
unknown <akwadtech.surveillance@yahoo.com>
2022-11-04 19:22:30 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeChallengeTaiwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num =  {5,3,6,1,12};  int  original =3;
+            foreach(int elements in num)
+            {   original++;
+                if (elements < original)
+                {
+                    Console.WriteLine(2*original);
+                }
+                else
+                {
+                    Console.WriteLine(original);
+                }
+            }
+
+        }
+    }
+}
